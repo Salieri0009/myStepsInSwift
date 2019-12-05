52c54db3bfa0e77e67ec36f0f64ded5dcb7409af
Maxim <nikulin.maxim0009@yandex.ru>
2019-12-05 19:49:47 +0500

    создание репозитория с моими заготовками.

---
+++ Custom UIButton/Custom UIButton/MyButton.swift
@@ -0,0 +1,21 @@
+//
+//  MyButton.swift
+//  Custom UIButton
+//
+//  Created by Maxim on 07/11/2019.
+//  Copyright © 2019 Maxim. All rights reserved.
+//
+
+import SwiftUI
+
+struct MyButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MyButton_Previews: PreviewProvider {
+    static var previews: some View {
+        MyButton()
+    }
+}
